db130f4b1f4491cb8bf2bedd136b1f7d2e3788d7
kulottunga <aryanx08@gmail.com>
2024-12-17 13:28:34 +0530

    Add a new print statement for feature branch

--- hello_world.py
+++ hello_world.py
@@ -16,8 +16,5 @@ print('SKELETONS')
 print('IKNOW')
 print('SICKO MODE')
 print('HER EYES')
-<<<<<<< HEAD
 print('This is the main branch change.')
-=======
-print('This is a conflicting change.')
->>>>>>> conflict-test
+print('Working on a new branch feature.')
